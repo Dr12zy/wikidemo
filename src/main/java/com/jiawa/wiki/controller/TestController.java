c8f174b9f39aec045d080b49a1d7560eb9974ff4
dr12zy <cst1909160@xmu.edu.my>
2022-05-20 03:21:52 +0800

    2-8 SpringBoot配置文件 自定义

--- src/main/java/com/jiawa/wiki/controller/TestController.java
+++ src/main/java/com/jiawa/wiki/controller/TestController.java
@@ -1,19 +1,21 @@
 package com.jiawa.wiki.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Map;
 
 @RestController
 
 public class TestController {
-@RequestMapping("/hello")
+    @Value("${test.hello:TEST}")
+    private String testHello;
+
+
+@GetMapping("/hello")
     public String hello() {
 
-        return "Hello world";
+        return "Hello world " + testHello;
     }
 
     @RequestMapping("/hello/post")
@@ -21,6 +23,7 @@ public class TestController {
 
         return "Hello world! Post, " + name;
     }
+
 //@PostMapping("/hello/post")
 //public String helloPost() {
 //
