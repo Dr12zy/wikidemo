5133dd140e7579f483bfab429eed7806616965d4
dr12zy <cst1909160@xmu.edu.my>
2022-05-20 02:45:36 +0800

    2-6开发Hello world 接口，修改port为8880 2-7增加hello接口测试test.http

--- src/main/java/com/jiawa/wiki/controller/TestController.java
+++ src/main/java/com/jiawa/wiki/controller/TestController.java
@@ -1,8 +1,11 @@
 package com.jiawa.wiki.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 
@@ -12,4 +15,16 @@ public class TestController {
 
         return "Hello world";
     }
+
+    @RequestMapping("/hello/post")
+    public String helloPost(String name) {
+
+        return "Hello world! Post, " + name;
+    }
+//@PostMapping("/hello/post")
+//public String helloPost() {
+//
+//    return "Hello world! Post, " + map.get("name");
+//}
+
 }
