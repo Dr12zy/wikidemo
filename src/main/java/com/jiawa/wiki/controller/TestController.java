6d33ae56282e36637885ca8b226cc76fcc9a4dac
dr12zy <cst1909160@xmu.edu.my>
2022-06-03 02:13:38 +0800

    3-5 将数据库改成wikidev

--- src/main/java/com/jiawa/wiki/controller/TestController.java
+++ src/main/java/com/jiawa/wiki/controller/TestController.java
@@ -1,18 +1,26 @@
 package com.jiawa.wiki.controller;
 
+import com.jiawa.wiki.domain.Test;
+import com.jiawa.wiki.service.TestService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import javax.annotation.Resource;
+import java.util.List;
 
+@RestController
 public class TestController {
     @Value("${test.hello:TEST}")
     private String testHello;
 
 
-@GetMapping("/hello")
+
+    @Resource
+    private TestService testService;
+
+    @GetMapping("/hello")
     public String hello() {
 
         return "Hello world " + testHello;
@@ -29,5 +37,10 @@ public class TestController {
 //
 //    return "Hello world! Post, " + map.get("name");
 //}
+@GetMapping("/test/list")
+public List<Test> list() {
+
+   return testService.list();
+}
 
 }
