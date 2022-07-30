1a7e4aead582f0e42f11d3c49b2b74e0a638cee6
dr12zy <cst1909160@xmu.edu.my>
2022-07-30 18:40:18 +0800

    4-3 使用Vue CLI生成web项目组

--- src/main/java/com/jiawa/wiki/service/EbookService.java
+++ src/main/java/com/jiawa/wiki/service/EbookService.java
@@ -6,7 +6,7 @@ import com.jiawa.wiki.domain.EbookExample;
 import com.jiawa.wiki.mapper.EbookMapper;
 import com.jiawa.wiki.req.EbookReq;
 import com.jiawa.wiki.resp.EbookResp;
-import org.springframework.beans.BeanUtils;
+import com.jiawa.wiki.util.CopyUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -29,12 +29,15 @@ public class EbookService {
 
         List<EbookResp> respList = new ArrayList<>();
         for (Ebook ebook : ebooklist) {
-            EbookResp ebookResp = new EbookResp();
-            BeanUtils.copyProperties(ebook, ebookResp);
-//             ebookResp.setId(123L);
+//            EbookResp ebookResp = new EbookResp();
+//            BeanUtils.copyProperties(ebook, ebookResp);
+
+            EbookResp ebookResp = CopyUtil.copy(ebook, EbookResp.class);
 
             respList.add(ebookResp);
         }
+
+        List<EbookResp> list = CopyUtil.copyList(ebooklist, EbookResp.class);
 
         return respList;
 
