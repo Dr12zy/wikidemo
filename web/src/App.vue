350b01ccb5b32aeea18b634081e5a66547c80fd0
dr12zy <cst1909160@xmu.edu.my>
2022-08-01 20:41:59 +0800

    4-6 网站首页布局的开发

--- web/src/App.vue
+++ web/src/App.vue
@@ -59,18 +59,13 @@
           </a-sub-menu>
         </a-menu>
       </a-layout-sider>
-      <a-layout style="padding: 0 24px 24px">
-        <a-breadcrumb style="margin: 16px 0">
-          <a-breadcrumb-item>Home</a-breadcrumb-item>
-          <a-breadcrumb-item>List</a-breadcrumb-item>
-          <a-breadcrumb-item>App</a-breadcrumb-item>
-        </a-breadcrumb>
+
         <a-layout-content
             :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
         >
           Content
         </a-layout-content>
-      </a-layout>
+
     </a-layout>
     <a-layout-footer style="text-align: center">
       Drizzy wiki demo
