91910053949788d3e4f4763eb9a7fbf15ee1e494
dr12zy <cst1909160@xmu.edu.my>
2022-08-02 16:44:28 +0800

    5-4 加入Ant Design Vue list组件     关闭@typescript-eslint/no-explicit-any规则校验,     集成图标组件

--- web/src/main.ts
+++ web/src/main.ts
@@ -4,7 +4,15 @@ import router from './router'
 import store from './store'
 import 'ant-design-vue/dist/antd.css';
 import Antd from 'ant-design-vue';
+import  * as Icons from '@ant-design/icons-vue';
+
 
 const app = createApp(App);
+    app.use(store).use(router).use(Antd).mount('#app');
 
-createApp(App).use(store).use(router).use(Antd).mount('#app')
+
+const icons: any = Icons;
+for( const i in icons){
+    app.component(i, icons[i]);
+}
+
